225652887c6e65c53510199ccf754803c60876f4
Chordun <chordunsetha@gmail.com>
2023-05-03 11:43:45 +0700

    origin fane

--- app.py
+++ app.py
@@ -14,7 +14,22 @@ def hello():
     message = "Hello War"
     return message
 
+@app.route('/get_prime', methods=['GET'])
+def get_prime():
+    prime = []
+    for i in range(1, 1001):
+        if is_prime(i):
+           prime.append(i)
 
+    return prime
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == '__main__':
     app.run(debug=True)
